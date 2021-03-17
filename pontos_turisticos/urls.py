9d719b305bf922635a118ccfba6984cf85d77438
newton perazzo <perazzoneto99@hotmail.com>
2021-03-17 11:04:48 -0300

    Commit até a aula '24'.

--- pontos_turisticos/urls.py
+++ pontos_turisticos/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
 from core.api.viewsets import PontoTuristicoViewSet
 from atracoes.api.viewsets import AtracoesViewSet
 from enderecos.api.viewsets import EnderecosViewSet
@@ -17,4 +19,4 @@ router.register(r'avaliacoes', AvaliacoesViewSet)
 urlpatterns = [
     path('', include(router.urls)),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
