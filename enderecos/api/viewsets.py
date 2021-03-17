5927811ba29806833cfccc36b74ca30dd06805be
newton perazzo <perazzoneto99@hotmail.com>
2021-03-17 09:16:37 -0300

    Commit até a aula '15'.

---
+++ enderecos/api/viewsets.py
@@ -0,0 +1,8 @@
+from rest_framework.viewsets import ModelViewSet
+from .serializers import EnderecoSerializer
+from enderecos.models import Endereco
+
+
+class EnderecosViewSet(ModelViewSet):
+    queryset = Endereco.objects.all()
+    serializer_class = EnderecoSerializer
