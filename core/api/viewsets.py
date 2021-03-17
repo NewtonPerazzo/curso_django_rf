5f8b22f0c95fa5782534a0e0fa544704b6f28a51
newton perazzo <perazzoneto99@hotmail.com>
2021-03-17 10:46:51 -0300

    Commit até a aula '23'.

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,3 +1,5 @@
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontoTuristico
 from .serializers import PontoTuristicoSerializer
@@ -7,3 +9,26 @@ class PontoTuristicoViewSet(ModelViewSet):
 
     def get_queryset(self):
         return PontoTuristico.objects.filter(aprovado=True)
+
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 'olá'})
+
+    def create(self, request, *args, **kwargs):
+        return Response({'Hello': request.data('nome')})
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass
+
+    @action(methods=['get'], detail=False)
+    def test(self, request):
+        pass
